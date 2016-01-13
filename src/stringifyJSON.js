ce5e45eaea93ffdfd1c1ab4dbc354c4163e931ed
Tritia Timmins <tritiatimmins@Tritias-MacBook-Pro.local>
2016-01-13 13:40:16 -0800

    Completes stringifyJSON, but working out some errors.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -17,7 +17,7 @@ var stringifyJSON = function(obj) {
   		} if (obj === false) {
   			return "false";
   		}
-  		
+
   	//can also do return obj ? "true" : "false"
   } if (typeof obj === "number") {
   		//toString() makes something a string
@@ -25,7 +25,60 @@ var stringifyJSON = function(obj) {
   
   	//if the object is a string then return the object 
   } if (typeof obj === "string") {
-  	return obj;
-  }
+  	
+  	//add a set of quotations around the obj
+  	return '"' + obj + '"';
+  
+  } if (typeof === null); {
+  	return "null";
 
+  	//if it's an array
+  	//if array.isarray of object
+  }	if (Array.isArray(obj)); {
+
+  	//overwrite the value of storage
+  	storage = "["
+  	//loop through the object
+  	for (var i = 0; obj.length; i ++) {
+  		//now overwrite the value of storage again
+  		//call it recursively 
+  		storage += stringifyJSON(obj[i]);
+  		
+  		//if i is less than the last position in the length of the obj, it will add commas to every value except the last one
+
+  		if (i < obj.length - 1) {
+  			storage += ","
+  		}
+
+  	} 
+  	//use plus equal because this will add it and not override the value of storage
+  		return storage += "]";
+
+  		//stringify objects
+  	} else {
+
+  		//make a new array and put the object into the array
+  		var objectCommaArray = [];
+
+  		storage = "{";
+
+  		for (var key in obj) {
+  			//if the obj[key] is not undefined or a function then we must push it into the storage [];
+
+  			if (obj[key] !== undefined && typeof obj[key] !== "function") {
+  				//push recursive function
+  				objectCommaArray.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+  			}
+  		}
+
+  		for (var i = 0; i < objectCommaArray.length; i ++) {
+
+  			storage += objectCommaArray[i];
+  			//if you're not at the last value don't add a comma
+  			if(i < objectCommaArray.length - 1) {
+  				storage += ","
+  			}
+  		}
+  			return storage += "}";
+  	} 	
 };
