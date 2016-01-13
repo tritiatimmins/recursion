f09a25c2171cf0cee05bb1b96ad29d8bb1dc8551
Tritia Timmins <tritiatimmins@Tritias-MacBook-Pro.local>
2016-01-13 13:18:07 -0800

    makes changes to stringify json omg this is hard

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,27 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  //save the objects into this thing
+
+  var storage = [];
+  // if object is a boolean
+  	if (typeof obj === "boolean") {
+  	//then return true or false
+  		if (obj === true) {
+  			return "true";
+  		} if (obj === false) {
+  			return "false";
+  		}
+  		
+  	//can also do return obj ? "true" : "false"
+  } if (typeof obj === "number") {
+  		//toString() makes something a string
+  	return obj.toString();
+  
+  	//if the object is a string then return the object 
+  } if (typeof obj === "string") {
+  	return obj;
+  }
+
 };
